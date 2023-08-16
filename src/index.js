b2764fb788c931d99d3512b9e78442efdbf73098
hafiz1379 <hafizrasa1379@gmail.com>
2023-08-16 10:33:50 +0430

    Solved linter errors

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const rootElement = document.getElementById('root');
+
+// Check if React 18 is available
+if (typeof ReactDOM.createRoot === 'function') {
+  // Use createRoot for React 18+
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  // Use ReactDOM.render for versions prior to React 18
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement
+  );
+}
